f9068c33ad0a04049de2518e5cfad060f3449ac7
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-08-01 13:52:48 +0200

    👌 IMPROVE: remove aria-pressed, add/fix event handlers

--- random-quote-generator/client/index.js
+++ random-quote-generator/client/index.js
@@ -20,23 +20,66 @@ async function getQuote(url) {
   }
 }
 
-getQuote(url).then((quote) => {
-  if (!!quote.data) {
-    if (quote.data.quoteAuthor.trim().length === 0) {
-      quoteAuthor.innerText = 'Unknown'
-    } else {
-      quoteAuthor.innerText = quote.data.quoteAuthor
-    }
+function getAndDisplayNewQuote(url) {
+  // check aria-pressed state of button
+  getQuote(url).then((quote) => {
+    if (!!quote.data) {
+      if (quote.data.quoteAuthor.trim().length === 0) {
+        quoteAuthor.innerText = 'Unknown'
+      } else {
+        quoteAuthor.innerText = quote.data.quoteAuthor
+      }
 
-    // reduce font size for long quotes for better
-    // container fitting
-    if (quote.data.quoteText.length > 120) {
-      quoteText.classList.add('long-quote')
-      quoteAuthor.classList.add('long-quote')
-    } else {
-      quoteText.classList.remove('long-quote')
-      quoteAuthor.classList.remove('long-quote')
+      // improve readability/style for long text
+      if (quote.data.quoteText.length > 120) {
+        quoteText.classList.add('long-quote')
+        quoteAuthor.classList.add('long-quote')
+      } else {
+        quoteText.classList.remove('long-quote')
+        quoteAuthor.classList.remove('long-quote')
+      }
+      quoteText.innerText = quote.data.quoteText
     }
-    quoteText.innerText = quote.data.quoteText
+  })
+}
+
+function buttonHandler(target, callback) {
+  callback()
+}
+
+function handleButtonClick(event, callback) {
+  buttonHandler(event.target, callback)
+}
+
+function handleButtonKeyDown(event, callback) {
+  // Check to see if space or enter were pressed
+  if (event.key === ' ' || event.key === 'Enter' || event.key === 'Spacebar') {
+    // "Spacebar" for IE11 support
+    // Prevent the default action to stop scrolling when space is pressed
+    event.preventDefault()
+    buttonHandler(event.target, callback)
   }
-})
+}
+
+// tweet
+function tweetQuote() {
+  const quote = quoteText.innerText
+  const author = quoteAuthor.innerText
+  const twitterUrl = `http://twitter.com/intent/tweet?text=${quote} - ${author}`
+  window.open(twitterUrl, '_blank')
+}
+
+// Event Listeners
+newQuoteButton.addEventListener('click', (e) =>
+  handleButtonClick(e, getAndDisplayNewQuote(url))
+)
+newQuoteButton.addEventListener('keydown', (e) =>
+  handleButtonKeyDown(e, getAndDisplayNewQuote(url))
+)
+twitterButton.addEventListener('click', (e) => handleButtonClick(e, tweetQuote))
+twitterButton.addEventListener('keydown', (e) =>
+  handleButtonKeyDown(e, tweetQuote)
+)
+
+// Get a quote on start
+getAndDisplayNewQuote(url)
